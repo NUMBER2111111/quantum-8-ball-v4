857548b07e4a9d22fb75becc1887fd250c8f76e4
NUMBER2111111 <RENEWENGINEERINGGROUP@GMAIL.COM>
2025-07-28 23:46:10 -0700

    Create App.tsx

---
+++ src/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Quantum 8-Ball Test</h1>
+      <p>If you can see this, the frontend is working!</p>
+    </div>
+  )
+}
+
+export default App
